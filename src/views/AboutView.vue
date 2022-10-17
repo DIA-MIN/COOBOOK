f42d73d2a81cd1310af30fa447372baa2bbea1b0
unknown <mns0327@naver.com>
2022-10-17 18:03:52 +0900

    :sparkles: test get recipe data

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div></div>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+  components: {},
+  data() {
+    return {
+      recipes: [],
+      options: {
+        method: 'GET',
+        url: `http://openapi.foodsafetykorea.go.kr/api/${process.env.VUE_APP_API_KEY}/COOKRCP01/json/1/1000`
+      }
+    }
+  },
+  setup() {},
+  created() {
+    this.getRecipes()
+  },
+  mounted() {},
+  unmounted() {},
+  methods: {
+    async getRecipes() {
+      try {
+        const response = await axios.request(this.options)
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
